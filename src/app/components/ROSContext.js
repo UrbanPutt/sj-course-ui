a28906d5c786d829effeb011ac22a9bff50b200a
Jake Gardner <jake@kurious-design.com>
2023-01-09 12:45:08 -0700

    modified the ws address for shop

--- src/app/components/ROSContext.js
+++ src/app/components/ROSContext.js
@@ -2,12 +2,13 @@ import React, { createContext, useState } from 'react'
 import PropTypes from 'prop-types'
 import ROSLIB from 'roslib'
 
-  //const startUrl: "ws://0.0.0.0:9090",
-  //const startUrl: "ws://jakers-ThinkPad.local:9090",
-  //const startUrl: "ws://ubuntu-sharkhole-raspi:9090",
-  //const startUrl: "ws://192.168.0.245:9090",
-  //const startUrl: "ws://192.168.0.36:9090",
-const startUrl = "ws://98.53.134.144:9090";
+//const startUrl: "ws://0.0.0.0:9090",
+//const startUrl: "ws://jakers-ThinkPad.local:9090",
+//const startUrl: "ws://ubuntu-sharkhole-raspi:9090",
+//const startUrl: "ws://192.168.0.245:9090",
+//const startUrl: "ws://192.168.0.36:9090",
+//const startUrl = "ws://98.53.134.144:9090"; //home
+const startUrl = "ws://174.29.180.255:9090"; //urban putt shop in denver
 
 const rosObj = {
   url: startUrl,
