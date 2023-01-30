e0da6a6858e262ad259ff03f3ae3cfad62b843ff
Jake Gardner <jake@kurious-design.com>
2023-01-30 14:06:04 -0700

    fixed starturl in roscontext

--- src/app/components/ROS/ROSContext.js
+++ src/app/components/ROS/ROSContext.js
@@ -5,9 +5,9 @@ import ROSLIB from 'roslib'
 //const startUrl: "ws://0.0.0.0:9090",
 //const startUrl: "ws://jakers-ThinkPad.local:9090",
 //const startUrl: "ws://ubuntu-sharkhole-raspi:9090",
-//const startUrl: "ws://192.168.0.245:9090",
+const startUrl = "ws://192.168.0.245:9090";
 //const startUrl: "ws://192.168.0.36:9090",
-const startUrl = "ws://98.53.134.144:9090"; //home
+//const startUrl = "ws://98.53.134.144:9090"; //home
 //const startUrl = "ws://174.29.180.255:9090"; //urban putt shop in denver
 
 const rosObj = {
